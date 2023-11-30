01fb6c122cd59fcbd30c237821cf05bcccfa5440
Aldo Santos <aldoacesso@gmail.com>
2023-11-30 12:20:46 -0300

    Organizado pastas e criado componentes

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/aldosds.png" alt="Foto usuário" />
+    <Titulo>Aldo Santos</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,15 @@
+import Titulo from '../../components/Titulo'
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre mim</Titulo>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+      excepturi perferendis incidunt dolor? Minima dicta maiores vel aspernatur
+      ratione fuga repellendus animi similique placeat? Aspernatur inventore
+      nesciunt nisi magnam dignissimos.
+    </p>
+  </section>
+)
+
+export default Sobre
