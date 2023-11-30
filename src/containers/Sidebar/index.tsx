18ecca9d6b3a76f90fbf9a7b3a5c2b605721fbe3
Aldo Santos <aldoacesso@gmail.com>
2023-11-30 15:47:38 -0300

    Criado componentes reutilizáveis

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -3,7 +3,7 @@ import Titulo from '../../components/Titulo'
 const Sidebar = () => (
   <aside>
     <img src="https://github.com/aldosds.png" alt="Foto usuário" />
-    <Titulo>Aldo Santos</Titulo>
+    <Titulo fontSize={20}>Aldo Santos</Titulo>
   </aside>
 )
 
--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,14 +1,15 @@
 import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
 
 const Sobre = () => (
   <section>
-    <Titulo>Sobre mim</Titulo>
-    <p>
+    <Titulo fontSize={16}>Sobre mim</Titulo>
+    <Paragrafo tipo="principal">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
       excepturi perferendis incidunt dolor? Minima dicta maiores vel aspernatur
       ratione fuga repellendus animi similique placeat? Aspernatur inventore
       nesciunt nisi magnam dignissimos.
-    </p>
+    </Paragrafo>
   </section>
 )
 
